8ff5cc9cf92e558ce0ea93e6e414f4d31618afef
Yetdev27 <yeisrael.dawit@aastustudent.edu.et>
2025-10-18 08:51:29 +0300

    coin info fetching function

--- server/index.js
+++ server/index.js
@@ -19,4 +19,40 @@ const {
     verifyTransaction
 } = crypto;
 
+const jobs = require('./jobs.js');
+const helpers = require('./helper.js');
+const { searchTickers } = helpers;
+
+const data = require("./tickers.json"); //coin info
+var tickers = {data}; // wrap in an object
+
+const cache = { feedback: {}}
+
+const coinGeckoClient = new CoinGecko();
+
+const second = 1000;
+const minute = 60 * second;
+
+const getCoin = async (token) => {
+    if (cache[token]) {
+        if (cache[token].timestamp - minute < Date.now()){
+            return cache[token]; // not sure if this logic work yet
+        }
+    }
+
+    const {data} = await coinGeckoClient.coins.fetch(token, {});
+    const name = data.name;
+    const {image, market_data, links } = data;
+    const {current_price} = market_data;
+
+    const out = {image, current_price, links, name,timestamp: Date.now() };
+
+    cache[token] = out;
+
+    return out;
+
+};
+
+
+
 
