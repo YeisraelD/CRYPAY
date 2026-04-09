99d831facbd7c86bc64fbf8ca4144c02ca069b68
YeisraelD <Yeisraeld@gmail.com>
2026-04-09 18:46:27 +0300

    request validation middleware in server/middleware/validator.js

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,7 +1,13 @@
 const AppError = require('../utils/appError');
 
+/**
+ * Middleware to validate the request body against a required schema
+ * @param {string[]} schema - Array of required field names
+ * @returns {Function} - Express middleware function
+ */
 const validate = (schema) => {
     return (req, res, next) => {
+        // Identify any fields present in the schema but missing from the request body
         const missingFields = schema.filter(field => !req.body[field]);
 
         if (missingFields.length > 0) {
